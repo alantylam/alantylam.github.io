8bb425311e524394e67e0e6295589511f06248c7
alantylam <alanlam56@gmail.com>
2019-05-31 01:33:22 -0600

    added details; added navbar; minor visual changes

--- JavaScript/new_design.js
+++ JavaScript/new_design.js
@@ -1,30 +1,50 @@
+window.onload = firstLn;
+
 var i = 0;
 var j = 0;
-var line_1 = ' Hello, World! ';
-var line_2 = ' My name is Alan. ';
-var speed = 150;
+var line_1 = ' Hi, my name is Alan. ';
+var line_2 = ' I am a Programmer.';
+var speed = 80;
 
 var i2 = 0;
 var j2 = 0;
 
-function typeWriter() {
+var pause = 900; 
+
+function firstLn() {
 	if (i < line_1.length) {
 		document.getElementById("message").innerHTML += line_1.charAt(i);
 		i++;
 		j = i;
-		setTimeout(typeWriter, speed);
+		setTimeout(firstLn, speed);
 	}
+	if (j > 1 && i == line_1.length) {
+		setTimeout(secLn, pause);
+	}
+}
+
+function secLn() {
 	if (j > 1 && i == line_1.length) {
 		document.getElementById("message").innerHTML = document.getElementById("message").innerHTML.replace(/(\s+)?.$/, '');
 		j--;
-		setTimeout(typeWriter, speed+200);
+		setTimeout(secLn, speed+30);
 	}
 	if (j == 1 && i == line_1.length) {
 		if (i2 < line_2.length) {
 			document.getElementById("message").innerHTML += line_2.charAt(i2);
 			i2++;
 			j2 = i2;
-			setTimeout(typeWriter, speed+400);
+			setTimeout(secLn, speed+140);
 		}
 	}
+	if (i2 == line_2.length) {
+		document.getElementById("learn-more").style.visibility = "visible";
+		document.getElementById("learn-more").style.color = "white";
+	}
 }
+
+function test() {
+	document.getElementById("learn-more").style.visibility = "visible";
+	document.getElementById("learn-more").style.color = "white";
+}
+
