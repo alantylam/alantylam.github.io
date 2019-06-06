229f1ccf1656c56c8ce3e8e958f23241362c388e
alantylam <alanlam56@gmail.com>
2019-06-05 23:46:07 -0600

    minor Visual Changes
    Added:
    -Message in loading screen
    
    Changed:
    -Loading screen
    
    TODO:
    -Parallax Effect
    -Logo Design

--- JavaScript/new_design.js
+++ JavaScript/new_design.js
@@ -35,8 +35,27 @@ $(document).ready(function(){
 		    $("#link-4").removeClass("active");
 		    //$('.link').css('color', 'white');
 		}
-
 	});
+
+	/*
+	$("a").on('click', function(event) {
+	    // Make sure this.hash has a value before overriding default behavior
+	    if (this.hash !== "") {
+	      	// Prevent default anchor click behavior
+	    	event.preventDefault();
+			// Store hash
+			var hash = this.hash;
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('body').animate({
+				scrollTop: $(hash).offset().top
+			}, 1400, function(){
+			// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
+	*/
 });
 
 var i = 0;
@@ -54,7 +73,7 @@ function trig() {
 	document.getElementById("container").style.opacity = "1";
 	document.getElementById("nav").style.opacity = "1";
 	document.getElementById("contact").style.opacity = "1";
-	setTimeout(firstLn, 700);
+	setTimeout(firstLn, 1400);
 }
 
 function firstLn() {
